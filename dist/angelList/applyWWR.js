8f97d06919cdf8d80e191f12100915c6f59dceb4
alexoid <alex_zam@hotmail.es>
2021-07-25 22:29:57 -0500

    add automated routes

--- dist/angelList/applyWWR.js
+++ dist/angelList/applyWWR.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const applyWWR = async (url, page) => {
     try {
         await page.goto(url);
+        await page.waitForSelector("form");
         const formnumber = await page.evaluate(() => {
             const forms = Array.from(document.querySelectorAll('form'));
             return forms.length;
